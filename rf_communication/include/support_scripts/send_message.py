04456c28e8014632f79c681bf52f66a0a765aee6
Harald <harald.luiks@gmail.com>
2019-09-30 14:00:05 +0900

    Set sending freq higher

--- rf_communication/include/support_scripts/send_message.py
+++ rf_communication/include/support_scripts/send_message.py
@@ -68,7 +68,7 @@ def main_programme():
 		#-------------------------------------------------
 		# Run the loop to send messages every 10 seconds
 		logger.info("Running for loop to send message.")
-		big_sleeptime = 10			# [seconds] Time between each message set
+		big_sleeptime = 3			# [seconds] Time between each message set
 		small_sleeptime = 0.2		# [seconds] Time between each message, this worked with a delay of 1, so now reducing to see performance
 
 		while(1):
